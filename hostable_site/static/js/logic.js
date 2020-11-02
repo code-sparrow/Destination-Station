bfc1561972b164ba378841c329969943f97804d5
code-sparrow <code.sparrow7@gmail.com>
2020-11-02 16:21:50 -0500

    Updated broken leaflet and mapbox links fixed new syntax

--- hostable_site/static/js/logic.js
+++ hostable_site/static/js/logic.js
@@ -105,10 +105,10 @@ markerLayer.eachLayer(function(layer) {
 });
 
 
-var streetMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
-    id: "mapbox.streets",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
 });
 
